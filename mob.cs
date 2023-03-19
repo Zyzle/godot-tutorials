e85fa0bf1fd9dc16e06dff6acbaf5ee669576f42
zyzle <colin.mcculloch@komi.io>
2023-03-19 03:10:35 +0000

    feat(mob): add mob scene and script

---
+++ mob.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class mob : RigidBody2D
+{
+	public override void _Ready()
+	{
+		var animSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animSprite2D.Play();
+		string[] mobTypes = animSprite2D.SpriteFrames.GetAnimationNames();
+		animSprite2D.Animation = mobTypes[GD.Randi() % mobTypes.Length];
+	}
+
+	public void OnVisibleOnScreenNotifier2DScreenExited()
+	{
+		QueueFree();
+	}
+}
